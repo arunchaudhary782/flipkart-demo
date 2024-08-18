b30c822aa2d00fa00a80066c46a27fd481a387a5
Arun <arunchaudhary782@gmail.com>
2024-08-18 21:48:14 +0530

    initial commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import { Container, Navbar, Nav, NavDropdown, Form, Button } from "react-bootstrap";
+import TopNavbar from "../../components/TopNavbar/TopNavbar";
+
+function Home(){
+
+    return (
+        <>
+            <TopNavbar/>
+            
+        </>
+    )
+}
+
+
+export default Home;
